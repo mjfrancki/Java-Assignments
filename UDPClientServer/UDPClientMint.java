f381c3e3d80a49ab5ad1679d35055b284e34a81b
mjfrancki <mikejfrancki@gmail.com>
2017-06-13 14:03:06 -0400

    Add files via upload
    Networks assignment

---
+++ UDPClientServer/UDPClientMint.java
@@ -0,0 +1,19 @@
+/*
+Assignment 2 CPS706 Fall 2015 
+Professor Isaac Woungang
+Christopher Iwanski 500567394
+Michael Francki 500554567
+UDPClientMint.java
+*/
+
+public class UDPClientMint{
+
+	public static void main (String[] args){
+
+
+
+		
+	}
+
+
+}
---
+++ UDPClientServer/UDPServerMint.java
@@ -0,0 +1,27 @@
+/*
+Assignment 2 CPS706 Fall 2015 
+Professor Isaac Woungang
+Christopher Iwanski 500567394
+Michael Francki 500554567
+UDPServerMint.java
+*/
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+
+public class UDPServerMint{
+
+	DatagramSocket serverSock;
+
+
+	public static void main (String[] args){
+
+
+
+
+	}
+
+
+}
